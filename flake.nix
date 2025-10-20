7f5524f0aa6b444f27b21861f8b582a23e5b7fc6
Anthony Rodriguez <anthony@nezia.dev>
2025-10-20 16:10:27 +0200

    chore(nix): update inputs; add treefmt

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
     self,
     nixpkgs,
     ...
-  } @ inputs: let
-    eachSystem = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+  }: let
+    forAllSystems = f: nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
     mkDate = longDate:
       with builtins; (concatStringsSep "-" [
         (substring 0 4 longDate)
@@ -14,12 +14,12 @@
     version = mkDate (self.lastModifiedDate or "19700101");
     src = self;
   in {
-    devShells = eachSystem (pkgs: {
+    devShells = forAllSystems (pkgs: {
       default = pkgs.mkShell {
         packages = with pkgs; [
           typst
-          typst-fmt
-          typst-lsp
+          tinymist
+          typstyle
         ];
         shellHook = let
           fonts = pkgs.lib.makeSearchPath "" [
@@ -33,13 +33,52 @@
       };
     });
 
-    packages = eachSystem (pkgs: {
+    packages = forAllSystems (pkgs: {
       english = pkgs.callPackage ./package.nix {
         inherit version src;
       };
       french = self.packages.${pkgs.system}.english.override {lang = "fr";};
       default = self.packages.${pkgs.system}.english;
     });
+
+    formatter = forAllSystems (pkgs:
+      pkgs.treefmt.withConfig {
+        settings = {
+          tree-root-file = "flake.nix";
+          formatter = {
+            alejandra = {
+              command = "alejandra";
+              includes = ["*.nix"];
+            };
+            deadnix = {
+              command = "deadnix";
+              options = ["--edit" "--no-lambda-arg" "--no-lambda-pattern-names"];
+              includes = ["*.nix"];
+            };
+            deno = {
+              command = "deno";
+              options = ["fmt"];
+              includes = ["*.md"];
+              excludes = ["*.css"];
+            };
+            typstyle = {
+              command = "typstyle";
+              options = ["--inplace"];
+              includes = ["*.typ"];
+            };
+          };
+        };
+
+        runtimeInputs = builtins.attrValues {
+          inherit
+            (pkgs)
+            deno
+            alejandra
+            deadnix
+            typstyle
+            ;
+        };
+      });
   };
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
